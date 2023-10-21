54bdb9fadbf73133a6e0758c7e188af612e51f5b
davidnedkonst <david.ned.kon@gmail.com>
2023-10-21 19:16:51 +0200

    Component Feedback. Step 1 completed.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Feedback from './components/Feedback';
 export default function App() {
   return (
     <div>
-      <h2>1. Feedback</h2>
+      <h1>React-HW-2</h1>
+
+      <h2>1. Component Feedback</h2>
       <Feedback />
     </div>
   );
--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,20 +1,54 @@
 import React from "react";
 
-export default class Feedback extends React.Component{
+export default class Feedback extends React.Component {
     state = {
         good: 0,
         neutral: 0,
-        bad: 0
+        bad: 0,
     };
 
-    render(){
+    onGoodLike = () => {
+        this.setState(prevState => ({
+            good: prevState.good + 1,
+            neutral: prevState.neutral,
+            bad: prevState.bad,
+        }))
+    };
+
+    onNeutralLike = () => {
+        this.setState(prevState => ({
+            good: prevState.good,
+            neutral: prevState.neutral + 1,
+            bad: prevState.bad,
+        }))
+    };
+
+    onBadLike = () => {
+        this.setState(prevState => ({
+            good: prevState.good,
+            neutral: prevState.neutral,
+            bad: prevState.bad + 1,
+        }))
+    };
+
+    render() {
+        const { good, neutral, bad } = this.state;
+
         return (
             <div>
                 <h3>Please leave feedback</h3>
+                <div>
+                    <button onClick={() => (this.onGoodLike())}>Good</button>
+                    <button onClick={() => (this.onNeutralLike())}>Neutral</button>
+                    <button onClick={() => (this.onBadLike())}>Bad</button>
+                </div>
 
-                
                 <h3>Statistics</h3>
-
+                <div>
+                    <p>Good: <span>{good}</span></p>
+                    <p>Neutral: <span>{neutral}</span></p>
+                    <p>Bad: <span>{bad}</span></p>
+                </div>
             </div>
         );
     };
